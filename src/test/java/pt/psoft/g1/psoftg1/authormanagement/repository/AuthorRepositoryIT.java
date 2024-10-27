2ec6139b9fac2a35f6fccfdde99c3aa3f982eacd
leilaboaze <1240470@isep.ipp.pt>
2024-10-27 03:25:13 +0000

    configure dependency injection

--- src/test/java/pt/psoft/g1/psoftg1/authormanagement/repository/AuthorRepositoryIT.java
+++ src/test/java/pt/psoft/g1/psoftg1/authormanagement/repository/AuthorRepositoryIT.java
@@ -1,7 +1,5 @@
 package pt.psoft.g1.psoftg1.authormanagement.repository;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
